5c4bd8394cba37a95b056241e9101db04a12b67b
jruben-roostify <38226316+jruben-roostify@users.noreply.github.com>
2018-08-22 14:48:39 -0700

    Update pipelineBuilder.groovy

--- vars/pipelineBuilder.groovy
+++ vars/pipelineBuilder.groovy
@@ -4,11 +4,29 @@ void init() {
 
 void gradleSteps(){
   try{
-  
+        stage ('Checkout') {
+              steps {
+                checkout scm
+              }
+        }
+        stage ('Clean') {
+            steps {
+                sh "./gradlew clean"
+            }
+        }
+        stage ('Build') {
+          steps {
+            sh "./gradlew build"
+          }
+        }
+        stage ('Test') {
+          steps {
+            sh "./gradlew test"
+          }
+        }
   }
   catch (e) {
-            currentBuild.result = "FAILED"
-            throw e
+    currentBuild.result = "FAILED"
+    throw e
   }
-
 }
