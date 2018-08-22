c361b8b7502aed3ae8fb1b299214ff6adc688527
jruben-roostify <38226316+jruben-roostify@users.noreply.github.com>
2018-08-22 14:56:34 -0700

    Update pipelineBuilder.groovy

--- vars/pipelineBuilder.groovy
+++ vars/pipelineBuilder.groovy
@@ -30,3 +30,21 @@ void gradleSteps(){
     throw e
   }
 }
+
+void sonarStep(){
+    stage ('Sonar'){
+        step {
+            script {
+                if (env.GIT_BRANCH == 'develop') {
+                    sh "./gradlew -b roostify-product-pricing/build.gradle sonar -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_PASSWORD}"
+                }else if (env.CHANGE_ID){
+                    echo 'This is a PR build. Running sonnar preview analysis'
+                    sh "./gradlew -b roostify-product-pricing/build.gradle sonar -Dsonar.github.pullRequest=${env.CHANGE_ID} -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_PASSWORD} -Dsonar.analysis.mode=preview -Dsonar.github.oauth=${GITHUB_OAUTH_TOKEN} -Dsonar.github.repository=${env.CUSTOM_SONAR_REPO_NAME} -i"
+                }else{
+                    echo 'This is a branch build.'
+                    sh "./gradlew -b roostify-product-pricing/build.gradle sonar -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_PASSWORD} -Dsonar.github.repository=${env.CUSTOM_SONAR_REPO_NAME} -Dsonar.branch=${env.GIT_BRANCH} -i"
+                }
+            }
+        }
+    }
+}
