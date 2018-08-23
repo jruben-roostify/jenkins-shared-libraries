763d904924e1d4760d6053c123f5607729f4c70d
jruben-roostify <38226316+jruben-roostify@users.noreply.github.com>
2018-08-22 18:24:57 -0700

    Create gradleSteps.groovy

---
+++ vars/gradleSteps.groovy
@@ -0,0 +1,36 @@
+def call(Map config) {
+    buildFilePath = ./build.gradle
+    if(config.containsKey('buildFilePath')){
+      buildFilePath = config.get('buildFilePath')
+    }
+    pipeline {
+      agent any
+        stage ('Checkout') {
+          steps {
+            checkout scm
+          }
+        }
+        stage ('Clean') {
+          steps {
+            sh "gradle -b ${buildFilePath} clean"
+          }
+        }
+        stage ('Build') {
+          steps {
+            sh "gradle -b ${buildFilePath} build"
+          }
+        }
+        
+          stage ('Test') {
+          when {
+            expression {
+              return ( config.containsKey('runTest') && config.get('runTest') )
+            }
+          }
+          steps {
+            sh "gradle -b ${buildFilePath} test"
+          }
+        }
+      }
+    }
+  }
