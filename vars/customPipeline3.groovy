55425f46d64ca04c2295c44b056944723c2162b6
jruben-roostify <38226316+jruben-roostify@users.noreply.github.com>
2018-08-22 22:27:59 -0700

    Update customPipeline3.groovy

--- vars/customPipeline3.groovy
+++ vars/customPipeline3.groovy
@@ -52,7 +52,7 @@ def call(Map config) {
                     sh "./gradlew -b "+buildFilePath+" sonar -Dsonar.github.pullRequest=${env.CHANGE_ID} -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_PASSWORD} -Dsonar.analysis.mode=preview -Dsonar.github.oauth=68eb8d979fe364931397bf39faa671159e5926ec -Dsonar.github.repository=${env.CUSTOM_SONAR_REPO_NAME} -i"
                 }else{
                     echo 'This is a branch build.'
-                    sh "./gradlew -b "+buildFilePath+" sonar -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_PASSWORD} -Dsonar.github.repository="+getRepoName()+ -Dsonar.branch=${env.GIT_BRANCH} -i"
+                    sh "./gradlew -b "+buildFilePath+" sonar -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_PASSWORD} -Dsonar.github.repository="+getRepoName()"+ -Dsonar.branch=${env.GIT_BRANCH} -i"
                 }
          }
     }
