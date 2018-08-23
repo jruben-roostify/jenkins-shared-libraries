3b93725825a5d4d1d85402039e66a08ebfa98aeb
jruben-roostify <38226316+jruben-roostify@users.noreply.github.com>
2018-08-22 18:52:15 -0700

    Update gradleSteps.groovy

--- vars/gradleSteps.groovy
+++ vars/gradleSteps.groovy
@@ -1,35 +1,31 @@
 def call(Map config) {
-    /*def buildFilePath = "./build.gradle"
-    if(config.containsKey('buildFilePath')){
-      buildFilePath = config.get('buildFilePath')
-    }*/
-    pipeline {
-      agent any
-        stage ('Checkout') {
-          steps {
-            checkout scm
-          }
-        }
-        stage ('Clean') {
-          steps {
-            sh "gradle clean"
-          }
-        }
-        stage ('Build') {
-          steps {
-            sh "gradle build"
-          }
-        }
-        
-          stage ('Test') {
-          when {
-            expression {
-              return ( config.containsKey('runTest') && config.get('runTest') )
-            }
-          }
-          steps {
-            sh "gradle test"
-          }
-        }
-      }
+ pipeline {
+  agent any
+  stage('Checkout') {
+   steps {
+    checkout scm
    }
+  }
+  stage('Clean') {
+   steps {
+    sh "gradle clean"
+   }
+  }
+  stage('Build') {
+   steps {
+    sh "gradle build"
+   }
+  }
+
+  stage('Test') {
+   when {
+    expression {
+     return (config.containsKey('runTest') && config.get('runTest'))
+    }
+   }
+   steps {
+    sh "gradle test"
+   }
+  }
+ }
+}
