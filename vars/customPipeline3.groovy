adaec505ce959d6642b8058c353278b68d898fde
jruben-roostify <38226316+jruben-roostify@users.noreply.github.com>
2018-08-22 20:02:16 -0700

    Update customPipeline3.groovy

--- vars/customPipeline3.groovy
+++ vars/customPipeline3.groovy
@@ -1,46 +1,48 @@
 def call(Map config) {
-    buildFilePath = "./build.gradle"
-    if(config.containsKey('buildFilePath')){
-      buildFilePath = config.get('buildFilePath')
+ buildFilePath = "./build.gradle"
+ if (config.containsKey('buildFilePath')) {
+  buildFilePath = config.get('buildFilePath')
+ }
+ pipeline {
+  agent any
+  stages {
+   stage('Checkout') {
+    steps {
+     checkout scm
     }
-    pipeline {
-      agent any
-       stages {
-        stage ('Checkout') {
-          steps {
-            checkout scm
-          }
-        }
-        stage ('Clean') {
-          steps {
-              sh "./gradlew -b ${buildFilePath} clean"
-          }
-        }
-        stage ('Build') {
-          steps {
-            sh "./gradlew -b ${buildFilePath} build"
-          }
-        }
-        
-        stage ('Test') {
-          when {
-            expression {
-              return ( config.containsKey('runTest') && config.get('runTest') )
-            }
-          }
-          steps {
-            sh "./gradlew -b ${buildFilePath} test"
-          }
-        }
-        stage ('Analysis'){
-            stages {
-                stage ('Code Coverage') {
-                    steps {
-                        jacoco buildOverBuild: true, changeBuildStatus: true
-                    }
-                }
-            }
-       }  
+   }
+   stage('Clean') {
+    steps {
+     sh "./gradlew -b ${buildFilePath} clean"
+    }
+   }
+   stage('Build') {
+    steps {
+     sh "./gradlew -b ${buildFilePath} build"
+    }
+   }
+
+   stage('Test') {
+    when {
+     expression {
+      return (config.containsKey('runTest') && config.get('runTest'))
+     }
+    }
+    steps {
+     sh "./gradlew -b ${buildFilePath} test"
+    }
+   }
+   parallel {
+    stage('Analysis') {
+     stages {
+      stage('Code Coverage') {
+       steps {
+        jacoco buildOverBuild: true, changeBuildStatus: true
+       }
       }
+     }
     }
+   }
+  }
+ }
 }
