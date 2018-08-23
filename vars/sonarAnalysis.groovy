e86d40bb5efd02252cefd00fa67e64e7f5be031b
jruben-roostify <38226316+jruben-roostify@users.noreply.github.com>
2018-08-22 21:59:04 -0700

    Update sonarAnalysis.groovy

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -5,14 +5,16 @@ def getRepoName(){
 }
 
 void sonarAnalysis(String buildFileLocation, Map config){
-  if (env.GIT_BRANCH == 'develop') {
-    println 'Performing full develop branch Analysis';
-    sh "./gradlew -b "+buildFileLocation+" sonar -Dsonar.host.url='${SONAR_URL}' -Dsonar.login='${SONAR_PASSWORD}'"
-  }else if (env.CHANGE_ID){
-    println 'Performing pull request Analysis';
-    sh "./gradlew -b "+buildFileLocation+" sonar -Dsonar.github.pullRequest='${env.CHANGE_ID}' -Dsonar.host.url='${SONAR_URL}' -Dsonar.login='${SONAR_PASSWORD}' -Dsonar.analysis.mode=preview -Dsonar.github.oauth=a9e63f6378746a280bf0756c2844d2266cca7dc9 -Dsonar.github.repository='${env.CUSTOM_SONAR_REPO_NAME}' -i"
-  }else{
-    println 'Running Sonar analysis on branch';
-    sh "./gradlew -b "+buildFileLocation+" sonar -Dsonar.host.url='${SONAR_URL}' -Dsonar.login="+getRepoName()+ -Dsonar.github.repository='${env.CUSTOM_SONAR_REPO_NAME}' -Dsonar.branch='${env.GIT_BRANCH}' -i"
-  }
+    script{
+        if (env.GIT_BRANCH == 'develop') {
+        println 'Performing full develop branch Analysis';
+        sh "./gradlew -b "+buildFileLocation+" sonar -Dsonar.host.url='${SONAR_URL}' -Dsonar.login='${SONAR_PASSWORD}'"
+        }else if (env.CHANGE_ID){
+            println 'Performing pull request Analysis';
+            sh "./gradlew -b "+buildFileLocation+" sonar -Dsonar.github.pullRequest='${env.CHANGE_ID}' -Dsonar.host.url='${SONAR_URL}' -Dsonar.login='${SONAR_PASSWORD}' -Dsonar.analysis.mode=preview -Dsonar.github.oauth=a9e63f6378746a280bf0756c2844d2266cca7dc9 -Dsonar.github.repository='${env.CUSTOM_SONAR_REPO_NAME}' -i"
+        }else{
+            println 'Running Sonar analysis on branch';
+            sh "./gradlew -b "+buildFileLocation+" sonar -Dsonar.host.url='${SONAR_URL}' -Dsonar.login="+getRepoName()+ -Dsonar.github.repository='${env.CUSTOM_SONAR_REPO_NAME}' -Dsonar.branch='${env.GIT_BRANCH}' -i"
+        }
+    }  
 }
