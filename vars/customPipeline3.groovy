c2f2159616a09138fa4a3295999a639d9b53a5d1
jruben-roostify <38226316+jruben-roostify@users.noreply.github.com>
2018-08-22 22:26:12 -0700

    Update customPipeline3.groovy

--- vars/customPipeline3.groovy
+++ vars/customPipeline3.groovy
@@ -40,7 +40,7 @@ def call(Map config) {
    stage('Sonar Analysis') {
     when {
      expression {
-      return (config.containsKey('runTest') && config.get('runTest'))
+      return (config.containsKey('sonarNeeded') && config.get('sonarNeeded'))
      }
     }
     steps {
@@ -52,11 +52,17 @@ def call(Map config) {
                     sh "./gradlew -b "+buildFilePath+" sonar -Dsonar.github.pullRequest=${env.CHANGE_ID} -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_PASSWORD} -Dsonar.analysis.mode=preview -Dsonar.github.oauth=68eb8d979fe364931397bf39faa671159e5926ec -Dsonar.github.repository=${env.CUSTOM_SONAR_REPO_NAME} -i"
                 }else{
                     echo 'This is a branch build.'
-                    sh "./gradlew -b "+buildFilePath+" sonar -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_PASSWORD} -Dsonar.github.repository=${env.CUSTOM_SONAR_REPO_NAME} -Dsonar.branch=${env.GIT_BRANCH} -i"
+                    sh "./gradlew -b "+buildFilePath+" sonar -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_PASSWORD} -Dsonar.github.repository="+getRepoName()+ -Dsonar.branch=${env.GIT_BRANCH} -i"
                 }
          }
     }
    }
   }
  }
+}
+
+def getRepoName(){
+    def repo = "${env.GIT_URL}"
+    repo_val = repo.replaceAll('https://github.com/', '').replaceAll('.git', '')
+    return repo_val;
 }
